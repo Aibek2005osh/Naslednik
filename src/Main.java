2c0aeada84d282ece494609cbb98eae5f4c96c8e
Aibek Dev <clesh.meder.271@gmail.com>
2024-11-10 18:41:10 +0600

    Inheritance

--- src/Main.java
+++ src/Main.java
@@ -5,7 +5,7 @@ public class Main {
         Person person = new Person("Adam Ata : ", " dyikan  ");
         Programmer programmer = new Programmer("Baiel : ", "programist : ", " peaksoft ");
         Dancer dancer = new Dancer("ernist : ", "Dancer", "BTS");
-        Singer singer = new Singer("Amir : " , "Dinner : ","biz tobu");
+        Singer singer = new Singer("Amir : " , "Singer: ","Кыз бурак ");
 
         Person[] name = new Person[]{person, programmer,dancer,singer};
         for (Person p : name) {
